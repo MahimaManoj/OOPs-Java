cd7f72be0f14b070f94655d588aa9bf0b61ce0b3
MahimaManoj <93909829+MahimaManoj@users.noreply.github.com>
2022-03-22 13:04:14 +0530

    Add files via upload

---
+++ Operations.java
@@ -0,0 +1,15 @@
+public class Operations{
+	public static void main(String args[]){
+		int a=10;
+		int b=5;
+		int add,sub,mul,div;
+		add=a+b;
+		sub=a-b;
+		mul=a*b;
+		div=a/b;
+		System.out.println("Addition: "+ add);
+		System.out.println("Subtraction: "+sub);
+		System.out.println("Multiplication: "+mul);
+		System.out.println("Division :"+div);
+	}
+}
